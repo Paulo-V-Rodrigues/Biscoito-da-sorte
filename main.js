0ea5baef2b1279dd60414890dbbd13d79e5d249a
Paulo Vitor <120144887+Paulo-V-Rodrigues@users.noreply.github.com>
2024-05-22 16:53:24 -0300

    mudança de tela JS

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
-function handClick{
-    event.preventDefault()
-    console.log("cheguei aqui")
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
+const randomNumber = math.round(Math.random() * 10)
+let xAttempts = 1
+
+function handleTryClick (event){
+    event.preventDefault() //não faça o padrao
+
+//função callback
+    const inputnumber = document.querySelector("#inputnumber")
+    
+    if(Number(inputnumber) == randomnumber) {
+
+        document
+        .querySelector(".screen2 h2")
+        .innerText `acertou em ${xAttempts} tentativas`
+    }
+
+    inputnumber.value = ""
+    xAttempts++
 }
+
+//eventos
+
+const btnTry = document.querySelector("#btnTry")
+const btnReaset = document.querySelector("btnReaset")
+
+btnTry.addEventListener('click', handleTryClick)
+btnReaset.addEventListener('click', function() {
+
+    screen1.classList.remove("hide")
+    screen2.classList.add("hide")
+
+    xAttempts = 1
+})
